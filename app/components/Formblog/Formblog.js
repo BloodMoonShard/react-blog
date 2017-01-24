dfa4033a0dc7475bbcce6f52bbdbdb595e454381
Андрей Андреевич Журавлев <AA2Zhuravlev.SBT@sberbank.ru>
2017-01-24 15:53:22 +0300

    Text

--- app/components/Formblog/Formblog.js
+++ app/components/Formblog/Formblog.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Link } from 'react-router'
-import { connect } from 'react-redux';
+import {Link} from 'react-router'
+import {connect} from 'react-redux'
 import {Button, Form, FormGroup, Col, FormControl, ControlLabel} from 'react-bootstrap'
 import _ from 'lodash'
-import { update } from '../../actions'
+import {update} from '../../actions'
 
 export class FormblogView extends React.Component {
 
@@ -15,13 +15,23 @@ export class FormblogView extends React.Component {
         _onSubmit: React.PropTypes.func
     }
 
+    static defaultProps = {
+        title: '',
+        description: ''
+    }
+
+    componentWillReceiveProps (newProps){
+        //TODO: Tear of my hands
+        window.history.back()
+    }
+
     onSubmit = () => {
-       const id = _.isUndefined(this.props.location.query.id) ? null : this.props.location.query.id
-       return this.props._onSubmit({
-           'id': id,
-           'title': ReactDOM.findDOMNode(this.refs.title).value,
-           'description': ReactDOM.findDOMNode(this.refs.description).value
-       })
+        const id = _.isUndefined(this.props.location.query.id) ? null : this.props.location.query.id
+        return this.props._onSubmit({
+            'id': id,
+            'title': ReactDOM.findDOMNode(this.refs.title).value,
+            'description': ReactDOM.findDOMNode(this.refs.description).value
+        })
     }
 
     render() {
@@ -29,11 +39,13 @@ export class FormblogView extends React.Component {
             <Col md={6} mdOffset={3}>
                 <Form horizontal onSubmit={this.onSubmit}>
                     <FormGroup controlId="formHorizontalEmail">
-                        <FormControl ref="title" type="text" placeholder="Title"/>
+                        <input className="form-control" defaultValue={this.props.title} ref="title" type="text"
+                                     placeholder="Title"/>
                     </FormGroup>
                     <FormGroup controlId="formControlsTextarea">
                         <ControlLabel>Description</ControlLabel>
-                        <FormControl ref="description" componentClass="textarea" placeholder="Description"/>
+                        <textarea className="form-control" ref="description" defaultValue={this.props.description}
+                                   placeholder="Description"/>
                     </FormGroup>
                     <Link className="pull-left btn btn-default" to="/">Cancel</Link>
                     <Button className="pull-right" onClick={this.onSubmit}>
@@ -46,9 +58,16 @@ export class FormblogView extends React.Component {
 }
 
 export const Formblog = connect(
-    function mapStateToProps(state) {
+    function mapStateToProps(state, props) {
+        if (!_.isNil(props.location.query.id)) {
+            const article = state.articles.find((item) => item.id === props.location.query.id)
+            return {
+                title: article.title,
+                description: article.description
+            }
+        }
         return {
-
+            title: '', description: ''
         }
     },
     function mapDispatchToProps(dispatch) {
@@ -56,5 +75,5 @@ export const Formblog = connect(
             _onSubmit: data => dispatch(update(data))
         }
     }
-)(FormblogView);
+)(FormblogView)
 
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -5,17 +5,17 @@ import _ from 'lodash'
 
 const initialState = iList([
     {
-        id: 0,
+        id: "0",
         title: 'Hello first item',
         description: 'Natoque amet augue elit pulvinar in montes sit dis montes tincidunt proin? Sed ultricies! Arcu. Ac. Aliquam adipiscing, augue turpis nec nec. In turpis! Rhoncus nascetur elementum placerat sed mattis adipiscing, natoque vel elementum natoque nec et placerat. Tincidunt, ridiculus elementum massa dignissim aliquet aenean, sed. Integer proin! Et enim urna et penatibus parturient, mus tempor sit non augue tincidunt odio ac. Eu porttitor risus tortor, platea amet urna pulvinar parturient adipiscing, adipiscing ultrices parturient non porta dignissim! Et magna. A scelerisque a enim, porttitor sit, sed pellentesque, cum mus augue! Turpis augue lundium, et tempor, tincidunt adipiscing lacus enim mauris penatibus in tortor et parturient amet dignissim? Cursus dapibus, dolor ac lorem urna et vel? Habitasse porta massa porttitor.'
     },
     {
-        id: 1,
+        id: "1",
         title: 'Hello first item2',
         description: 'Natoque amet augue elit pulvinar in montes sit dis montes tincidunt proin? Sed ultricies! Arcu. Ac. Aliquam adipiscing, augue turpis nec nec. In turpis! Rhoncus nascetur elementum placerat sed mattis adipiscing, natoque vel elementum natoque nec et placerat. Tincidunt, ridiculus elementum massa dignissim aliquet aenean, sed. Integer proin! Et enim urna et penatibus parturient, mus tempor sit non augue tincidunt odio ac. Eu porttitor risus tortor, platea amet urna pulvinar parturient adipiscing, adipiscing ultrices parturient non porta dignissim! Et magna. A scelerisque a enim, porttitor sit, sed pellentesque, cum mus augue! Turpis augue lundium, et tempor, tincidunt adipiscing lacus enim mauris penatibus in tortor et parturient amet dignissim? Cursus dapibus, dolor ac lorem urna et vel? Habitasse porta massa porttitor.'
     },
     {
-        id: 5,
+        id: "5",
         title: 'Hello first item3',
         description: 'Natoque amet augue elit pulvinar in montes sit dis montes tincidunt proin? Sed ultricies! Arcu. Ac. Aliquam adipiscing, augue turpis nec nec. In turpis! Rhoncus nascetur elementum placerat sed mattis adipiscing, natoque vel elementum natoque nec et placerat. Tincidunt, ridiculus elementum massa dignissim aliquet aenean, sed. Integer proin! Et enim urna et penatibus parturient, mus tempor sit non augue tincidunt odio ac. Eu porttitor risus tortor, platea amet urna pulvinar parturient adipiscing, adipiscing ultrices parturient non porta dignissim! Et magna. A scelerisque a enim, porttitor sit, sed pellentesque, cum mus augue! Turpis augue lundium, et tempor, tincidunt adipiscing lacus enim mauris penatibus in tortor et parturient amet dignissim? Cursus dapibus, dolor ac lorem urna et vel? Habitasse porta massa porttitor.'
     }
@@ -31,8 +31,9 @@ export const articles = (state = initialState, action) => {
             }))
             if(_.isNil(action.data.id)){
                 action.data.id = maxId+1;
+                return state.push(action.data)
             }
-            return state.push(action.data)
+            return state.setIn(state.findIndex((item)=>item.id === action.data.id).toString(), action.data)
         default:
             return state
     }
